887e5535f7c142e90853bb93bdec53f9b9714f12
André Garcia Dobermann <32962579+andredob@users.noreply.github.com>
2019-10-19 19:33:05 -0300

    chopTable almost working
    need to  add the reference sprite to all animals, tack the points and make the cut puzzes

--- Dont Mess With My Blend/Scripts/ChopTable/ChopTable.gd
+++ Dont Mess With My Blend/Scripts/ChopTable/ChopTable.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+#simplified phats
+var snakePath = "table/Snake"
+var frogPath = "table/Frog"
+var batPath = "table/Bat"
+var spiderPath = "table/Spider"
+
+#cut vars
 var cutting = false
 var cut = false
 
+#point vars
 var prevPoint = Vector2(0,0)
 var dist = 0
 var actualPoint
 
+#calculate the distance between two points and add a new point to the cut
 func _process(delta):
 	actualPoint = get_global_mouse_position()
 	if cut:
@@ -19,6 +28,8 @@ func _process(delta):
 		pass
 	pass
 
+
+#makes that just when the mouse is pressed is possible to cut
 func _input(event):
 	if Input.is_action_just_pressed("Cut") and cutting:
 		print(get_global_mouse_position())
@@ -28,8 +39,25 @@ func _input(event):
 	pass
 	
 
-#Frog button
-func _on_Button_button_down():
-	get_child(1).get_child(0).visible = true
+
+#randonly select the sprite to be show for the selected animal
+func _on_Button_button_down(animalName):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var rand = rng.randi_range(0,4)
+	print(rand)
+	if animalName == "frog":
+		get_node(frogPath).frame = rand
+		get_node(frogPath).visible = true
+	elif animalName == "bat":
+		get_node(batPath).frame = rand
+		get_node(batPath).visible = true
+	elif animalName == "snake":
+		get_node(snakePath).frame = rand
+		get_node(snakePath).visible = true
+	elif animalName == "spider":
+		get_node(spiderPath).frame = rand
+		get_node(spiderPath).visible = true
 	cutting = true
-	pass # Replace with function body.
+	pass
+
