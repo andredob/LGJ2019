1ebb235ef8d8380d9090b3602326292a074392f2
Mikeask <maycon-adm@hotmail.com>
2019-10-20 15:02:22 -0300

    pegar items do armário para o inventário

--- Dont Mess With My Blend/Scripts/Armario.gd
+++ Dont Mess With My Blend/Scripts/Armario.gd
@@ -6,16 +6,16 @@ var AguaBanho = preload("res://Sprites/Ingredientes/AguaBanho.png")
 var Cristal = preload("res://Sprites/Ingredientes/Cristal.png")
 var PoDeGuarana = preload("res://Sprites/Ingredientes/PoDeGuarana.png")
 var SementeDePapoula = preload("res://Sprites/Ingredientes/SementeDePapoula.png")
-var SuorGamer = preload("res://Sprites/Ingredientes/Calcio.png")
-var Calcio = preload("res://Sprites/Ingredientes/AguaBanho.png")
+var SuorGamer = preload("res://Sprites/Ingredientes/SuorDeGamer.png")
+var Calcio = preload("res://Sprites/Ingredientes/Calcio.png")
 
 var itens = [
-{"nome": "Pó de camumila", "sprite": PoCamomila, "tipo":"ingrediente", "tamanho" : 40},
+{"nome": "Pó de camomila", "sprite": PoCamomila, "tipo":"ingrediente", "tamanho" : 40},
 {"nome": "Semente de maracujá", "sprite": SementeMaracuja,  "tipo":"ingrediente", "tamanho": 80},
 {"nome": "Agua de banho", "sprite": AguaBanho, "tipo":"ingrediente", "tamanho":100},
 {"nome": "Cristal", "sprite": Cristal, "tipo":"ingrediente", "tamanho" : 16},
 {"nome": "Pó de guaraná", "sprite": PoDeGuarana, "tipo":"ingrediente", "tamanho" : 20},
-{"nome": "Semente de Papoula", "sprite": SementeDePapoula, "tipo":"ingrediente", "tamanho" : 12},
+{"nome": "Semente de papoula", "sprite": SementeDePapoula, "tipo":"ingrediente", "tamanho" : 12},
 {"nome": "Suor de gamer", "sprite": SuorGamer, "tipo":"ingrediente", "tamanho" : 24},
 {"nome": "Calcio", "sprite": Calcio, "tipo":"ingrediente", "tamanho":140}
 ]
