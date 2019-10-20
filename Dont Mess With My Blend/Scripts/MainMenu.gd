bb3d7b07b18ea262dbcf8ba52954e57db1b460a0
Mikeask <maycon-adm@hotmail.com>
2019-10-20 16:26:50 -0300

    mudança na UI (textura)

--- Dont Mess With My Blend/Scripts/MainMenu.gd
+++ Dont Mess With My Blend/Scripts/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 # Declare member variables here. Examples:
 onready var circleEdge = get_node("CircleEdge")
 onready var circleCenter = get_node("CircleCenter")
-
+var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
@@ -11,6 +11,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	time = time + delta
+	print(time)
+	if(time>4):
+		var surgeRuna = get_node("TextureRect/SurgeRuna")
+		surgeRuna.position = Vector2((randi()%1180)+100, (randi()%620)+100)
+		surgeRuna.get_node("AnimationPlayer").play("BrilhoAnim")
+		time=0
+	
 	circleEdge.set_rotation(circleEdge.get_rotation()+0.002)
 	circleCenter.set_rotation(circleCenter.get_rotation()-0.0002)
 	pass    
